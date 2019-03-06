ca9d1b3f4a8a3c2615b4ce8896232fe17292e832
UBJOJO <jjh1004ss@gmail.com>
2019-03-06 13:35:01 +0900

    data type

--- c_programming/Graffe/main.c
+++ c_programming/Graffe/main.c
@@ -3,14 +3,9 @@
 
 int main()
 {
-    char characterName[] = "John";
-    int characterAge = 38;
-    printf("There once was a man named %s\n", characterName);
-    printf("he was %d years old.\n", characterAge);
-
-    characterAge = 30;
-    printf("He really liked the name %s\n", characterName);
-    printf("but did not like being %d.\n", characterAge);
-
+    int age = 40;
+    double gpa = 3.6;
+    char grade = 'Hcdf';
+    char phrase[] = "Graff ca";
     return 0;
 }
