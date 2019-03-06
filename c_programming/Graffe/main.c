a4ee6bba915b8f9e1bf07a5a74dc30577110832e
UBJOJO <jjh1004ss@gmail.com>
2019-03-06 13:24:30 +0900

    var

--- c_programming/Graffe/main.c
+++ c_programming/Graffe/main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char characterName[] = "John";
+    int characterAge = 38;
+    printf("There once was a man named %s\n", characterName);
+    printf("he was %d years old.\n", characterAge);
+
+    characterAge = 30;
+    printf("He really liked the name %s\n", characterName);
+    printf("but did not like being %d.\n", characterAge);
+
     return 0;
 }
